96a2714bca2c93527d2d61fcac01a8630d629e93
Bogdan Tretyakov <bogdan@tretyakov.dev>
2025-02-07 19:57:54 +0300

    fix: links dialog

--- components/IconLink.vue
+++ components/IconLink.vue
@@ -1,19 +1,21 @@
 <template>
-  <v-tooltip location="top">
-    <template #activator="{ props }">
-      <v-btn
-        :href="link"
-        v-bind="props"
-        variant="plain"
-        rel="nofollow"
-        target="_blank"
-      >
-        <v-icon v-if="typeof icon === 'string'" :icon="icon" />
-        <font-awesome-icon v-else :icon="icon" />
-      </v-btn>
-    </template>
-    <span><slot /></span>
-  </v-tooltip>
+  <client-only>
+    <v-tooltip location="top">
+      <template #activator="{ props }">
+        <v-btn
+          :href="link"
+          v-bind="props"
+          variant="plain"
+          rel="nofollow"
+          target="_blank"
+        >
+          <v-icon v-if="typeof icon === 'string'" :icon="icon" />
+          <font-awesome-icon v-else :icon="icon" />
+        </v-btn>
+      </template>
+      <span><slot /></span>
+    </v-tooltip>
+  </client-only>
 </template>
 
 <script setup lang="ts">
