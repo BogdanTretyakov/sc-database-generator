4e4d0acf06c1f0acd377ac1fe1bc6d66177a91bf
Bogdan Tretyakov <bogdan@tretyakov.dev>
2025-04-05 16:25:29 +0300

    chore: oz triggers

--- components/triggers/OzMainTrigger.vue
+++ components/triggers/OzMainTrigger.vue
@@ -11,37 +11,37 @@
     <template #body>
       <tr>
         <td>70</td>
-        <td>-5</td>
-        <td>-</td>
-        <td>-</td>
-      </tr>
-      <tr>
-        <td>80</td>
         <td>-10</td>
         <td>-</td>
         <td>-</td>
       </tr>
       <tr>
+        <td>80</td>
+        <td>-20</td>
+        <td>-</td>
+        <td>-</td>
+      </tr>
+      <tr>
         <td>90</td>
-        <td>-15</td>
+        <td>-30</td>
         <td>+10%</td>
         <td>-</td>
       </tr>
       <tr>
         <td>100</td>
-        <td>-20</td>
+        <td>-40</td>
         <td>+20%</td>
         <td>+30%</td>
       </tr>
       <tr>
         <td>110</td>
-        <td>-25</td>
+        <td>-50</td>
         <td>+30%</td>
         <td>+60%</td>
       </tr>
       <tr>
         <td>120</td>
-        <td>-40</td>
+        <td>-60</td>
         <td>+40%</td>
         <td>+90%</td>
       </tr>
