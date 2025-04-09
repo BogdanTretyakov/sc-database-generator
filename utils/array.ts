8eb24b446132735395a1b0cbf2659008e2a157b5
Bogdan Tretyakov <bogdan@tretyakov.dev>
2025-04-09 16:35:18 +0300

    chore: og 4.26.1

--- utils/array.ts
+++ utils/array.ts
@@ -1,5 +1,7 @@
 import { isNotNil } from './guards';
 
+const collator = new Intl.Collator();
+
 export function uniq<T>(item: T, index: number, array: T[]): boolean {
   return array.indexOf(item) === index;
 }
@@ -17,9 +19,9 @@ export function sortVersion(items: string[]) {
     .map((ver) => ver.match(/\d+|\w/g))
     .filter(isNotNil)
     .sort(([a1, a2, a3], [b1, b2, b3]) => {
-      if (a1 !== b1) return Number(b1) - Number(a1);
-      if (a2 !== b2) return Number(b2) - Number(a2);
-      if (a3 || b3) return Number(Boolean(b3)) - Number(Boolean(a3));
+      if (a1 !== b1) return collator.compare(b1, a1);
+      if (a2 !== b2) return collator.compare(b2, a2);
+      if (a3 || b3) return collator.compare(b3 ?? '', a3 ?? '');
       return 0;
     })
     .map((parts) => parts.join('.').replace(/\.(?!\d)/, ''));
