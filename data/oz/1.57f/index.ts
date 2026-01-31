d407fca736a3acf951098edffb3610f91c07cf04
Bogdan Tretyakov <bogdan@tretyakov.dev>
2026-01-31 16:12:27 +0300

    chore: reborn 1.57f

---
+++ data/oz/1.57f/index.ts
@@ -0,0 +1,30 @@
+import type { IDataFile } from '~/data/types';
+import type { VersionIndexFile } from '~/data';
+
+const racesData = Object.entries(
+  import.meta.glob<any>('./*.json', { import: 'default', eager: false })
+).reduce((acc, [key, value]) => {
+  const path = key.split(/\\|\/|\./g);
+  const name = path[path.length - 2];
+  acc[name] = value as () => Promise<IDataFile>;
+  return acc;
+}, {} as Record<string, () => Promise<IDataFile>>);
+
+const racesIcons = Object.entries(
+  import.meta.glob<any>('./*.webp', { import: 'default', eager: true })
+).reduce((acc, [key, value]) => {
+  const path = key.split(/\\|\/|\./g);
+  const name = path[path.length - 2];
+  acc[name] = value as string;
+  return acc;
+}, {} as Record<string, string>);
+
+const version = '1.57f';
+const versionType = 'oz';
+
+export default {
+  racesData,
+  racesIcons,
+  version,
+  versionType,
+} as VersionIndexFile;
